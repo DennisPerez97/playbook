96664b0bc2fb23df0ab20f71cd1f16044b857abe
DennisPerez97 <dennis.perez.97@hotmail.com>
2022-04-28 20:49:23 -0500

    Agregando requerimiento de fechas

--- weekly_mission_3/twitter/test/models/User.test.js
+++ weekly_mission_3/twitter/test/models/User.test.js
@@ -1,13 +1,20 @@
 const User = require('./../../app/models/User')
 
 describe("Pruebas de unidad de usuario", () =>{
-    test('Caso de prueba 1: Creacion de objeto', () =>{
+    test('Caso de prueba 1: Creacion de User', () =>{
        // expect(3).toBe(10)
         const usuario = new User(10, "Miniempoleon", "Dennis Perez","Bio")
         expect(usuario.id).toBe(10);
         expect(usuario.name).toBe("Dennis Perez")
         expect(usuario.user).toBe("Miniempoleon");
         expect(usuario.bio).toBe("Bio");
-        expect(usuario.dateCreated).toBe(new Date())
     });
+
+
+    test('Caso de prueba 2: Revision de las fechas deUser', () =>{
+        // expect(3).toBe(10)
+         const usuario = new User(10, "Miniempoleon", "Dennis Perez","Bio")
+         expect(usuario.dateCreated).not.toBeUndefined()
+         expect(usuario.lastUpdate).not.toBeUndefined()
+     });
 })
